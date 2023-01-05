d95ac320c9038540dd5576e013e61fe0c14f9947
unknown <preshwhyte1@gmail.com>
2023-01-05 13:39:46 +0100

    jait

--- smart/settings.py
+++ smart/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ma74i_%gq&rto_aasji9zfiiu%2ulo)m542t0akw^s7ui0l%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
